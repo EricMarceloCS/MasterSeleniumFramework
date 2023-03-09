8302956da2b86155b01b8c714e50ac5cff13fdab
eric <ericmarcelo455@gmail.com>
2023-03-09 09:54:38 -0800

    File Path
    * Change Path String '/' to File.separator

--- master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/MyFirstTestCase.java
+++ master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/MyFirstTestCase.java
@@ -4,11 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 public class MyFirstTestCase {
 
     @Test
     public void dummyTest() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "src"
+                + File.separator
+                + "main"
+                + File.separator
+                + "resources"
+                + File.separator
+                + "drivers"
+                + File.separator
+                + "chromedriver");
+
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://askomdch.com/");
         // webDriver.get("https://www.saucedemo.com/");
