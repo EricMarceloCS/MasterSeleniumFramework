aab09aea997a853a68b4d9701de8d283b424af7e
eric <ericmarcelo455@gmail.com>
2023-03-15 12:53:00 -0700

    Automate Driver Management
    * Add bonigarcis webdrivermanager to pom
    * Change System.setProperty to WebDriverManger.cachePath()
    * Cache drivers in test/resources

--- master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/factory/DriverManager.java
+++ master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/factory/DriverManager.java
@@ -1,26 +1,32 @@
 package dev.ericmarcelo.selenium.pom.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.File;
-import java.time.Duration;
 
 public class DriverManager {
 
     public WebDriver initializeDriver() {
-        System.setProperty("webdriver.gecko.driver", "src"
-                + File.separator
-                + "main"
-                + File.separator
-                + "resources"
-                + File.separator
-                + "drivers"
-                + File.separator
-                + "geckodriver");
+
+        WebDriverManager
+                .firefoxdriver()
+                .cachePath("src"
+                        + File.separator
+                        + "test"
+                        + File.separator
+                        + "resources"
+                        + File.separator
+                        + "drivers"
+                        + File.separator
+                        + "geckodriver")
+                .setup();
+
         WebDriver webDriver = new FirefoxDriver();
         webDriver.manage().window().maximize();
-        // webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         return webDriver;
     }
 }
