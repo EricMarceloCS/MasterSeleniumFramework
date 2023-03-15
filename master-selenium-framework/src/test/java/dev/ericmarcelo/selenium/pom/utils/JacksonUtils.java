8862fb5caff8ba0c8f5a1c7695a464813b469c06
eric <ericmarcelo455@gmail.com>
2023-03-15 10:14:51 -0700

    Test Optimizations
    * Change ObjectMapper to custom BillingAddress Mapper
    * Select unable to open dropdown menu
    * Remove Application State dependency
    * Remove User state dependency

--- master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/utils/JacksonUtils.java
+++ master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/utils/JacksonUtils.java
@@ -1,16 +1,45 @@
 package dev.ericmarcelo.selenium.pom.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.ericmarcelo.selenium.pom.objects.BillingAddress;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JacksonUtils {
 
-    public static BillingAddress deserializeJson(InputStream is, BillingAddress billingAddress) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-       return objectMapper.readValue(is, billingAddress.getClass());
+    private static Map<String, String> mapper = new HashMap<>();
+    public static BillingAddress deserializeBillingAddressJson(InputStream is, BillingAddress billingAddress) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        mapBillingAddress(br);
+        setBillingAddress(billingAddress);
+       return billingAddress;
+    }
+
+    private static void mapBillingAddress(BufferedReader br) throws IOException {
+        String str;
+        while((str = br.readLine()) != null){
+            if(str.length() == 1)
+                continue;
+            String[] strings = str.split("\"");
+            mapper.put(strings[1], strings[3]);
+        }
+    }
+
+    private static void setBillingAddress(BillingAddress billingAddress){
+        billingAddress.setFirstName(mapper.get("firstName"))
+                .setLastName(mapper.get("lastName"))
+                .setCountry(mapper.get("country"))
+                .setAddress(mapper.get("address"))
+                .setCity(mapper.get("city"))
+                .setState(mapper.get("state"))
+                .setPostalCode(mapper.get("postalCode"))
+                .setEmail(mapper.get("email"))
+                .setUserName(mapper.get("userName"))
+                .setPassword(mapper.get("password"));
+
+        mapper.clear();
     }
 }
