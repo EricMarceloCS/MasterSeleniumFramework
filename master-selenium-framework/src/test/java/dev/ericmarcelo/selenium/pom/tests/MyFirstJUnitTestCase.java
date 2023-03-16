68d048b3cb928ceebd6ca680e0a3df43db02a33f
eric <ericmarcelo455@gmail.com>
2023-03-16 13:42:13 -0700

    JUnit Jupiter
    * Add junit-jupiter-engine to pom.xml
    * Add MyFirstJUnitTestCase in tests package
    * Add BaseJUnitTest in base package

---
+++ master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/tests/MyFirstJUnitTestCase.java
@@ -0,0 +1,89 @@
+package dev.ericmarcelo.selenium.pom.tests;
+
+import dev.ericmarcelo.selenium.pom.base.BaseJUnitTest;
+import dev.ericmarcelo.selenium.pom.objects.BillingAddress;
+import dev.ericmarcelo.selenium.pom.objects.User;
+import dev.ericmarcelo.selenium.pom.pages.CartPage;
+import dev.ericmarcelo.selenium.pom.pages.CheckoutPage;
+import dev.ericmarcelo.selenium.pom.pages.HomePage;
+import dev.ericmarcelo.selenium.pom.pages.StorePage;
+import dev.ericmarcelo.selenium.pom.utils.BillingJacksonUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyFirstJUnitTestCase extends BaseJUnitTest {
+
+    @Test
+    public void dummyTest() {
+
+        webDriver.get("https://askomdch.com/");
+        //webDriver.get("https://www.saucedemo.com/");
+        // webDriver.get("https://demoqa.com/");
+    }
+
+    @Test
+    public void guestCheckoutUsingDirectBankTransfer() throws IOException {
+
+        BillingAddress billingAddress = new BillingAddress();
+        InputStream is = getClass().getClassLoader().getResourceAsStream("BillingAddress.json");
+        BillingJacksonUtils.deserializeBillingAddressJson(is, billingAddress);
+
+        StorePage storePage = new HomePage(webDriver)
+                .load().navigateToStoreUsingMenu()
+                .search("Blue");
+        Assertions.assertEquals("Search results: “Blue”", storePage.getTitle());
+
+        storePage.clickAddToCartButton("Blue Shoes");
+        CartPage cartPage = storePage.clickViewCart();
+        Assertions.assertEquals("Blue Shoes", cartPage.getProductName());
+
+        CheckoutPage checkoutPage = cartPage.checkout();
+        checkoutPage.setBillingAddress(billingAddress)
+                .selectDirectBankTransfer()
+                .placeOrder();
+
+        Assertions
+                .assertEquals("Thank you. Your order has been received."
+                        , checkoutPage.getNotice());
+
+    }
+    @Test
+    public void loginAndCheckoutUsingDirectBankTransfer() {
+
+        StorePage storePage = new HomePage(webDriver)
+                .load().navigateToStoreUsingMenu()
+                .search("Blue");
+        Assertions.assertEquals("Search results: “Blue”", storePage.getTitle());
+
+        storePage.clickAddToCartButton("Blue Shoes");
+        CartPage cartPage = storePage.clickViewCart();
+        Assertions.assertEquals("Blue Shoes", cartPage.getProductName());
+
+        CheckoutPage checkoutPage = cartPage.checkout();
+        checkoutPage.enterLogin();
+
+        User user = new User("demouser2", "demopwd");
+
+        BillingAddress billingAddress = new BillingAddress()
+                .setFirstName("demo")
+                .setLastName("user")
+                .setAddress("San Francisco")
+                .setCity("San Francisco")
+                .setPostalCode("94188")
+                .setEmail("askomdch@gmail.com")
+                .setUserName(user.getUserName())
+                .setPassword(user.getPassword());
+
+        checkoutPage.setBillingAddress(billingAddress);
+        checkoutPage.setUser(user);
+        checkoutPage.login();
+        checkoutPage.placeOrder();
+
+        Assertions
+                .assertEquals("Thank you. Your order has been received."
+                        , checkoutPage.getNotice());
+    }
+}
