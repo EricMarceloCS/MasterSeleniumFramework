30d989e82dd3711a44fc2bdcf18eae15ecf0dbcd
eric <ericmarcelo455@gmail.com>
2023-03-23 08:11:11 -0700

    Atomic Add To Cart
    * Add addToCartFromProductPage

--- master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/pages/ProductPage.java
+++ master-selenium-framework/src/test/java/dev/ericmarcelo/selenium/pom/pages/ProductPage.java
@@ -3,11 +3,16 @@ package dev.ericmarcelo.selenium.pom.pages;
 import dev.ericmarcelo.selenium.pom.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class ProductPage extends BasePage {
 
 
     private final By productTitle = By.cssSelector(".product_title");
+    private final By addToCartButton = By.cssSelector(".single_add_to_cart_button");
+    private final By message = By.cssSelector(".woocommerce-message");
     public ProductPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -19,6 +24,15 @@ public class ProductPage extends BasePage {
 
     public String getTitle() {
         return webDriver.findElement(productTitle).getText();
+    }
+
+    public ProductPage clickAddToCartButton() {
+        webDriver.findElement(addToCartButton).click();
+        return this;
+    }
+
+    public List<WebElement> getMessage() {
+        return webDriver.findElements(message);
     }
 
 }
